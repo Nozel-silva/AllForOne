91fa7eb5df24de92d6cab50eaae62d7ce23a0c63
Nuel Agafie <70712577+Nozel-silva@users.noreply.github.com>
2026-03-22 20:48:46 +0100

    Update video.js

--- api/video.js
+++ api/video.js
@@ -14,8 +14,8 @@ export default async function handler(req, res) {
       });
 
       res.setHeader('Content-Type', 'video/mp4');
-      res.setHeader('Content-Disposition', 'attachment; filename="video.mp4"');
-
+      const filename = req.query.filename || 'video.mp4';
+      res.setHeader('Content-Disposition', `attachment; filename="${filename}"`);
       const buffer = await videoRes.arrayBuffer();
       res.send(Buffer.from(buffer));
       return;
