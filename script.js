3befa0edc37edf5f584cea2322c72573ab0d431d
Nuel Agafie <70712577+Nozel-silva@users.noreply.github.com>
2026-03-22 17:17:29 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const SUPABASE_URL = 'https://ccmsjcnuyrngqxwrswfe.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNjbXNqY251eXJuZ3F4d3Jzd2ZlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE2MjU2MjgsImV4cCI6MjA2NzIwMTYyOH0.dkjCo2bgDMf923VKESkyMLsULo7IhmsYb6r-4Dn6SRY';
-
 const fetchBtn = document.getElementById('fetchBtn');
 const tweetUrlInput = document.getElementById('tweetUrl');
 const results = document.getElementById('results');
